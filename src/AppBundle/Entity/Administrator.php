2c80c9008399b1b98b3d876e4a37e3030ace23c1
skowron-line <wygladaszjak@gmail.com>
2016-09-25 15:41:22 +0200

    fosuserbundle

---
+++ src/AppBundle/Entity/Administrator.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace AppBundle\Entity;
+
+/**
+ * @author Krzysztof Skaradziński <skaradzinski.krzysztof@gmail.com>
+ */
+class Administrator extends \FOS\UserBundle\Model\User
+{
+
+}
