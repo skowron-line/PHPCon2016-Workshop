d97806c23e3aa384563979d9dd305b10541e0f4f
skowron-line <wygladaszjak@gmail.com>
2016-09-26 08:49:14 +0200

    new speaker + new workshop

--- src/AppBundle/Controller/WorkshopController.php
+++ src/AppBundle/Controller/WorkshopController.php
@@ -4,11 +4,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Attendee;
+use AppBundle\Entity\Speaker;
 use AppBundle\Entity\Workshop;
 use AppBundle\Events\AttendeeAddedEvent;
 use AppBundle\Form\Dto\RegisterDto;
 use AppBundle\Form\Type\CreateType;
 use AppBundle\Form\Type\RegisterType;
+use AppBundle\Form\Type\SpeakerType;
 use AppBundle\WorkshopEvents;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,10 +37,72 @@ class WorkshopController extends Controller
             ]
         );
 
+        if (true === $request->isMethod('POST')) {
+            $createForm->handleRequest($request);
+
+            $workshop->setDisplay(true);
+
+            $manager = $this
+                ->getDoctrine()
+                ->getManager();
+
+            $manager->persist($workshop);
+            $manager->flush();
+
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add(
+                    'success',
+                    sprintf('Workshop "%s" successfully added', $workshop->getTitle())
+                );
+
+            return $this->redirectToRoute('list');
+        }
+
         return $this->render(
             'Workshop/create.html.twig',
             [
                 'createForm' => $createForm->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function speakerAction(Request $request)
+    {
+        $speaker = new Speaker();
+        $speakerForm = $this->createForm(SpeakerType::class, $speaker);
+
+        if (true === $request->isMethod('POST')) {
+            $speakerForm->handleRequest($request);
+
+            $manager = $this
+                ->getDoctrine()
+                ->getManager();
+
+            $manager->persist($speaker);
+            $manager->flush();
+
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add(
+                    'success',
+                    sprintf('Speaker "%s" successfully added', $speaker->getFullName())
+                );
+
+            return $this->redirectToRoute('list');
+        }
+
+        return $this->render(
+            'Workshop/speaker.html.twig',
+            [
+                'speakerForm' => $speakerForm->createView(),
             ]
         );
     }
---
+++ src/AppBundle/Form/Type/SpeakerType.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Entity\Speaker;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Krzysztof Skaradziński <skaradzinski.krzysztof@gmail.com>
+ */
+class SpeakerType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('about')
+            ->add('submit', SubmitType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => Speaker::class,
+            ]
+        );
+    }
+
+}
